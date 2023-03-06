e1f6c0f02648e1bad500f54b2447c1b4a3afd886
黄振庭 <21371024@buaa.edu.cn>
2023-03-06 19:30:19 +0800

    11111

--- exam.sh
+++ exam.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+mkdir mydir
+chmod 777 mydir
+touch myfile
+echo 2023 > myfile
+mv moveme mydir
+cp copyme mydir
+mv mydir/copyme mydir/copied
+cat readme
+gcc bad.c 2> err.txt
 a=1
 b=10
 if [ $# -gt 1]
