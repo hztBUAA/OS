82f574ff7f035b03677698ce732da8f329a4c9e9
黄振庭 <21371024@buaa.edu.cn>
2023-03-06 20:49:34 +0800

    11

--- analyze.sh
+++ analyze.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
-
-if [ $# -eq 2 ]
+if [ $# -eq 1 ]
 then
     # Your code here. (1/4)
-	grep "WARN|ERROR" $1 > bug.txt
+	grep "WARN\|ERROR"  $1 > bug.txt
 else
     case $2 in
     "--latest")
         # Your code here. (2/4)
-	tail -n, --lines=5 $1 
+	tail -n , --lines=3 $1 
     ;;
     "--find")
         # Your code here. (3/4)
-	grep $3 > $3.txt
+	grep $3 $1  > $3.txt
     ;;
     "--diff")
         # Your code here. (4/4)
-	diff $1 $3
+	diff -q $1 $3 > /dev/null
 	if [ $? -eq 0]
 	then
 		echo same
