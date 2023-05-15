e5ce5d119af6ec94b705c5eb8273f8a182db068b
黄振庭 <21371024@buaa.edu.cn>
2023-05-15 19:23:00 +0800

    “2”

--- user/lib/ipc.c
+++ user/lib/ipc.c
@@ -37,3 +37,36 @@ u_int ipc_recv(u_int *whom, void *dstva, u_int *perm) {
 
 	return env->env_ipc_value;
 }
+
+
+
+
+
+u_int get_time(u_int *us){
+	int time = 0;
+	int timee = 0;
+    if (syscall_read_dev((u_int) & time, 0x15000000, 4) < 0)
+        user_panic("time_read panic");
+    if (syscall_read_dev((u_int) & time, 0x15000010, 4) < 0)
+        user_panic("time_read panic");
+    if (syscall_read_dev((u_int) & timee, 0x15000010, 4) < 0)
+        user_panic("time_read panic");
+    *us = timee;
+    return time;
+}
+
+void usleep(u_int us) {
+	// 读取进程进入 usleep 函数的时间
+	u_int timee = 0;
+	u_int starttime = get_time(&timee);
+	while (1) {
+		// 读取当前时间
+		u_int curtime = get_time(&timee);
+		if ((int)curtime-(int)starttime >= us/1000000+1) {
+			return;
+		} else {
+			syscall_yield();
+			// 进程切换
+		}
+	}
+}
